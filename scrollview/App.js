a2babe30ab97f5fb479621494a2b67eff98d39ac
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2023-05-26 18:35:36 -0700

    scrollview

---
+++ scrollview/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <ScrollView style={styles.scrollView}>
+    <SafeAreaView style={styles.container}>
+        <Text style={{fontSize: 20, backgroundColor: 'tomato', margin: 10, padding: 10, borderRadius: 15}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+        <Text style={{fontSize: 20, backgroundColor: '#1ECBE1', margin: 10, padding: 10, borderRadius: 15}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+        <Text style={{fontSize: 20, backgroundColor: '#CCE11E', margin: 10, padding: 10, borderRadius: 15}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+        <Text style={styles.referencia}>Referência: https://reactnative.dev/docs/scrollview</Text>
+    </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  referencia: {
+    padding: 15
+  }
+});
+
+export default App;
